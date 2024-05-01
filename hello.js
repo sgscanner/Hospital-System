45533cb60c2387ffe9b18daca19ab006cffc54bb
sgscanner <117415489+sgscanner@users.noreply.github.com>
2024-05-01 09:15:17 -0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -9,4 +9,4 @@ const obj = { "api_key": "b1c4a89e-4905-5e3c-b57f-dc92627d011z"}
 const solana_token_key = "3n2C45yBiNapKh6EdDekesZccAFdwTatu46Eyw1UnKx2"
 
 
-const obj = { "solana_token_key": "3n2C45yBiNapKh6EdDekesZccAFdwTatu46Eyw1UnKx2"}
+const obj1 = { "solana_token_key": "3n2C45yBiNapKh6EdDekesZccAFdwTatu46Eyw1UnKx2"}
