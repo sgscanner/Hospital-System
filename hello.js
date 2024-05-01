db941818b464f07568dbb4868c424cebe28bfd8b
sgscanner <117415489+sgscanner@users.noreply.github.com>
2024-05-01 09:11:07 -0700

    Create hello.js
    test

---
+++ hello.js
@@ -0,0 +1,12 @@
+const api_key = "b1c4a89e-4905-5e3c-b57f-dc92627d011z"
+
+
+const obj = { "api_key": "b1c4a89e-4905-5e3c-b57f-dc92627d011z"}
+
+
+
+
+const solana_token_key = "3n2C45yBiNapKh6EdDekesZccAFdwTatu46Eyw1UnKx2"
+
+
+const obj = { "solana_token_key": "3n2C45yBiNapKh6EdDekesZccAFdwTatu46Eyw1UnKx2"}
